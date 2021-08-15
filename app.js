8c10443bec926b2c1b5233d3346a026d62e72a6c
Maruthi Pothuganti <maruthicharypothuganti@gmail.com>
2021-08-15 11:48:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var userInput = document.querySelector("#input");
+var btnTranslate = document.querySelector("#translate");
+var outputMessage = document.querySelector("#output");
+
+
+var translationURL = "	https://api.funtranslations.com/translate/shakespeare.json";
+
+function getTranslation(textInput) {
+    return translationURL + "?text=" + textInput;
+}
+
+function errorHandle(error) {
+    console.log("error message", error);
+    alert("Please try after Some time");
+}
+
+btnTranslate.addEventListener("click", function translateText() {
+    var text = userInput.value;
+
+    fetch(getTranslation(text)).then(response => response.json()).then(json => {
+        var translatedText = json.contents.translated;
+        outputMessage.innerText = translatedText;
+    }).
+    catch(errorHandle)
+});
